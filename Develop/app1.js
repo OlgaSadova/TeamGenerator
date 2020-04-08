a8d5617a5dc361f3cb6613f2cae290f4c4b24226
Olga Sadova <sadovaolga1@gmail.com>
2020-04-07 22:32:13 -0700

    Finalize Code

--- Develop/app1.js
+++ Develop/app1.js
@@ -1,117 +1,126 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
-//const OUTPUT_DIR = path.resolve(__dirname, "output");
-//const outputPath = path.join(OUTPUT_DIR, "team.html");​
-const render = require("./lib/htmlRenderer");​​
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const teamMembers = [];
 
 
+function askForEmployee() {
+    inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "What is your name?"
 
-var teamMembers = [];
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email?"
+        },
+        {
+            type: "list",
+            message: "What is your occupation?",
+            name: "occupation",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern"
+            ]
+        }
+    ]).then(dealWithAnswers);
+}
 
-//var engineeringTeam = createEngineeringTeam();
-//employee.push(engineeringTeam);
+function dealWithAnswers(answers) {
+    var newOccupation;
 
-
-//console.log(employee)
-
-//function createEngineeringTeam() {
-//function buildTeam() {
-//   if (!fs.existsSync(OUTPUT_DIR)) {
-//    fs.mkdirSync(OUTPUT_DIR)
-// }
-// fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
-//}
-return inquirer.prompt([{
-        type: "input",
-        name: "name",
-        message: "Whai is your name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your id?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email?"
-    },
-    {
-        type: "list",
-        message: "What is your occupation?",
-        name: "occupation",
-        choices: [
-            "Manager",
-            "Engineer",
-            "Intern"
-        ]
-    }
-]).then(answers => {
-    let newOccupation;
     switch (answers.occupation) {
         case 'Manager':
-            newOccupation = new Manager(answers.name, answers.id, answers.email);
+            inquirer.prompt([{
+                type: "input",
+                name: "OfficeNumber",
+                message: "What is the Office Number?"
+            }]).then(mgmtAnswer => {
+                (answers.name, answers.id, answers.email, mgmtAnswer.OfficeNumber);
+                buildEmployee(new Manager(answers.name, answers.id, answers.email, mgmtAnswer.OfficeNumber))
+
+
+            });
+
             break;
         case 'Engineer':
-            newOccupation = new Engineer(answers.name, answers.id, answers.email);
+            inquirer.prompt([{
+                type: "input",
+                name: "github",
+                message: "What is your gitHub account?"
+            }]).then(engAnswer => {
+
+
+                buildEmployee(new Engineer(answers.name, answers.id, answers.email, engAnswer.github));
+
+            })
+
             break;
         case 'Intern':
-            newOccupation = new Intern(answers.name, answers.id, answers.email);
+            inquirer.prompt([{
+                type: "input",
+                name: "School",
+                message: "What is the School?"
+            }]).then(intAnswer => {
+
+                buildEmployee(new Intern(answers.name, answers.id, answers.email, intAnswer.School));
+
+            });
+
             break
         default:
             break;
     }
-    if (answers.occupation === 'Manager') {
-        inquirer.prompt([{
-            type: "input",
-            name: "OfficeNumber",
-            message: "What is the Office Number?"
-        }]).then(mgmtAnswer => {
-            newOccupation.addOfficeNumber(mgmtAnswer.OfficeNumber);
-            //console.log(newOccupation)
-        })
+}
 
-    }
-    if (answers.occupation === 'Engineer') {
-        inquirer.prompt([{
-            type: "input",
-            name: "github",
-            message: "What is your gitHub account?"
-        }]).then(engAnswer => {
-            newOccupation.setGitHub(engAnswer.github);
-            //console.log(newOccupation)
-        })
+function saveToFile() {
 
-    }
-    if (answers.occupation === 'Intern') {
-        inquirer.prompt([{
-            type: "input",
-            name: "School",
-            message: "What is the School?"
-        }]).then(intAnswer => {
-            newOccupation.setSchool(intAnswer.School);
-            //console.log(newOccupation)
-        })
-    }
-    teamMembers.push(newOccupation);
+    const html = render(teamMembers);
+    console.log(html);
+    fs.writeFile(outputPath, html, err => {
+        if (err) throw err;
+    });
+}
 
-    //function buildTeam() {
-    //   if (!fs.existsSync(OUTPUT_DIR)) {
-    //   fs.mkdirSync(OUTPUT_DIR)
-    // }
-    // fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
-    //}
+function buildEmployee(employee) {
+    teamMembers.push(employee);
+    askForMoreEmployee();
+}
 
-    console.log(teamMembers)
-        //render(teamMembers);
+function askForMoreEmployee() {
+    inquirer
+        .prompt([{
 
-    //render(teamMembers);
+            type: 'confirm',
+            name: 'moreEmployee',
+            message: 'Would you like to enter another employee?',
+            default: true
 
-    //buildTeam();
-    //return newOccupation
+        }])
+        .then(answers => {
+            if (answers.moreEmployee) {
+                askForEmployee();
+            } else {
+                saveToFile();
+            }
+        });
+}
 
-})
+askForEmployee();
